3d4aa6286d6e2adbab2ef4d59d61765da5d6d022
Aaron Willisford <aaronwillisford@gmail.com>
2021-03-18 06:36:07 -0400

    made methods classmethods

--- data_generator.py
+++ data_generator.py
@@ -20,47 +20,49 @@ class data_generator():
     count_test_hotdog = 0
     count_test_nothotdog = 0
 
-    def gen_training_data():
-        LABELS = {data_generator.TRAIN_HOTDOG: 0, data_generator.TRAIN_NOTHOTDOG: 1} # hotdogs set to 0, else 1
+    @classmethod # class methods modify class state that applies across all instances of the class
+    def gen_training_data(cls):
+        LABELS = {cls.TRAIN_HOTDOG: 0, cls.TRAIN_NOTHOTDOG: 1} # hotdogs set to 0, else 1
         for label in LABELS:
             for file in tqdm(os.listdir(label)): # create list from files in label, and iterates through list
                 file_path = os.path.join(label, file)
                 image = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE) # load a color image in grayscale
-                data_generator.training_data.append([np.array(image), LABELS[label]]) # group image to correct label
+                cls.training_data.append([np.array(image), LABELS[label]]) # group image to correct label
 
                 # count data to keep balance
-                if label == data_generator.TRAIN_HOTDOG:
-                    data_generator.count_train_hotdog += 1
-                if label == data_generator.TRAIN_NOTHOTDOG:
-                    data_generator.count_train_nothotdog += 1
+                if label == cls.TRAIN_HOTDOG:
+                    cls.count_train_hotdog += 1
+                if label == cls.TRAIN_NOTHOTDOG:
+                    cls.count_train_nothotdog += 1
 
-        np.random.shuffle(data_generator.training_data)
-        np.save("data/training_data.npy", data_generator.training_data) # save data to a .npy file
+        np.random.shuffle(cls.training_data)
+        np.save("data/training_data.npy", cls.training_data) # save data to a .npy file
 
         # output count of each image
-        print("Training hotdogs:", data_generator.count_train_hotdog)
-        print("Training not hotdogs:", data_generator.count_train_nothotdog)
+        print("Training hotdogs:", cls.count_train_hotdog)
+        print("Training not hotdogs:", cls.count_train_nothotdog)
 
-    def gen_testing_data():
-        LABELS = {data_generator.TEST_HOTDOG: 0, data_generator.TEST_NOTHOTDOG: 1} # hotdogs set to 0, else 1
+    @classmethod # class methods modify class state that applies across all instances of the class
+    def gen_testing_data(cls):
+        LABELS = {cls.TEST_HOTDOG: 0, cls.TEST_NOTHOTDOG: 1} # hotdogs set to 0, else 1
         for label in LABELS:
             for file in tqdm(os.listdir(label)): # create list from files in label, and iterates through list
                 file_path = os.path.join(label, file)
                 image = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE) # load a color image in grayscale
-                data_generator.testing_data.append([np.array(image), LABELS[label]]) # group image to correct label
+                cls.testing_data.append([np.array(image), LABELS[label]]) # group image to correct label
 
                 # count data to keep balance
-                if label == data_generator.TEST_HOTDOG:
-                    data_generator.count_test_hotdog += 1
-                if label == data_generator.TEST_NOTHOTDOG:
-                    data_generator.count_test_nothotdog += 1
+                if label == cls.TEST_HOTDOG:
+                    cls.count_test_hotdog += 1
+                if label == cls.TEST_NOTHOTDOG:
+                    cls.count_test_nothotdog += 1
 
-        np.random.shuffle(data_generator.testing_data)
-        np.save("data/testing_data.npy", data_generator.testing_data) # save data to a .npy file
+        np.random.shuffle(cls.testing_data)
+        np.save("data/testing_data.npy", cls.testing_data) # save data to a .npy file
 
         # output count of each image
-        print("Testing hotdogs:", data_generator.count_test_hotdog)
-        print("Testing not hotdogs:", data_generator.count_test_nothotdog)
+        print("Testing hotdogs:", cls.count_test_hotdog)
+        print("Testing not hotdogs:", cls.count_test_nothotdog)
 
 data_generator.gen_training_data()
 data_generator.gen_testing_data()
