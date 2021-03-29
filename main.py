d2707849a9175d3c5de2b144e28e54639b8da5b4
Aaron Willisford <aaronwillisford@gmail.com>
2021-03-29 00:38:25 -0400

    added tensor saving

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 from Model import Model
 import torch.optim as optim
-import tqdm
+from tqdm import tqdm
+import numpy as np
+import torch
 
 ''' https://pytorch.org/tutorials/beginner/blitz/neural_networks_tutorial.html '''
 
 def main():
     net = Model()
 
-    BATCH_SIZE = 100
-    for epoch in range(1):
-        for batch in tqdm(range(0, len(training_data), BATCH_SIZE)):
+    training_data = np.load("data/training_data.npy", allow_pickle=True)
+
+    SAVE_DATA = False
+
+    if SAVE_DATA == True:
+        features = [x[0] for x in training_data]
+        labels = [x[1] for x in training_data]
+
+        X = torch.tensor(features)
+        y = torch.tensor(labels)
+
+        torch.save(X, 'features.pt')
+        torch.save(y, 'labels.pt')
+    else:
+        X = torch.load('features.pt')
+        y = torch.load('labels.pt')
+
+    # BATCH_SIZE = 100
+    # for epoch in range(1):
+    #     for batch in tqdm(range(0, len(training_data), BATCH_SIZE)):
+    #         pass
 
 if __name__ == "__main__":
     main()
